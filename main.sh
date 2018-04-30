084f4418619a8fe74fb0f4b6f76fa2c2170f4475
Howlin Bash <howlinbash@posteo.de>
2018-04-30 23:57:15 +0100

    Rewrite bashscript for multiboard

--- main.sh
+++ main.sh
@@ -1,46 +1,59 @@
 #!/bin/bash
 
+root="$HOME/.todo"
+board="$root/boards/$1.todo"
+alias=$2
+bashrc="$HOME/.bash_aliases"
+vimrc="$HOME/.vim/vimrc.d/mappings.vim"
+
 function print_aliases() {
-   for a in $(cat ~/.bash_aliases | grep todo/boards | cut -d '=' -f 1 | cut -d ' ' -f 2); do
+   for a in $(cat $bashrc | grep todo/boards | cut -d '=' -f 1 | cut -d ' ' -f 2); do
        echo $a;
    done
 }
-
-if [ "$1" == "list" ]; then
-    print_aliases
-    exit 1
-fi
 
 function alias_exists_error() {
     echo 'Alias already exists, pick a new one'
     exit 1
 }
 
-if [[ $2 ]]; then
-    # if alias already exists exit the program
-    type $2 &>/dev/null && alias_exists_error
-    echo "alias $2='cd ~/.todo/boards/$1 && $EDITOR index.todo'" >> ~/.bash_aliases
-    echo "nmap <Leader>$2 :call DynamicSplit('~/.todo/boards/$1/index.todo')<CR>" >> ~/.vim/vimrc.d/mappings.vim
-else
-    type $1 &>/dev/null && alias_exists_error
-    echo "alias $1='cd ~/.todo/boards/$1 && $EDITOR index.todo'" >> ~/.bash_aliases
-    echo "nmap <Leader>$1 :call DynamicSplit('~/.todo/boards/$1/index.todo')<CR>" >> ~/.vim/vimrc.d/mappings.vim
+function check_todo_is_uniq() {
+   for a in $(cat $bashrc | grep todo/boards | cut -d '=' -f 1 | cut -d ' ' -f 2); do
+       if [ "$a" == "$1" ]; then
+           echo "Todo list already exists, pick a new alias or type '$1'"
+           exit 1
+       fi
+   done
+}
+
+
+if [ "$1" == "list" ]; then
+    print_aliases
+    exit 1
 fi
 
-board=$1
-len=${#board} 
+check_todo_is_uniq  $1
 
+if [[ $alias ]]; then
+    # if alias already exists exit the program
+    type $alias &>/dev/null && alias_exists_error
+    echo "alias $alias='\$EDITOR $board'" >> $bashrc
+    echo "nmap <Leader>$alias :call DynamicSplit('$board')<CR>" >> $vimrc
+else
+    type $1 &>/dev/null && alias_exists_error
+    echo "alias $1='\$EDITOR $board'" >> $bashrc
+    echo "nmap <Leader>$1 :call DynamicSplit('$board')<CR>" >> $vimrc
+fi
+
+len=${#1} 
 i=1
 while [ "$i" -le "$len" ]; do
     underline+="="
     i=$(($i + 1))
 done
 
-mkdir ~/.todo/boards/$board
-cd ~/.todo/boards/$board
-mkdir archive
-touch index.todo archive/archive_index.todo
-echo $board > index.todo
-echo $underline >> index.todo
-echo '' >> index.todo
-$EDITOR index.todo
+touch $board
+echo $1 > $board
+echo $underline >> $board
+echo '' >> $board
+$EDITOR $board
